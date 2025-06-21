fe954ae018c61bc55c66830ac4a512b4514be8a4
maxtor1234 <maxtor1234@qq.com>
2025-06-21 13:13:41 +0800

    Update TestInitialization.java

--- TestInitialization.java
+++ TestInitialization.java
@@ -2,88 +2,67 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 
-import org.opennms.protocols.icmp6.ICMPv6EchoReply;
-import org.opennms.protocols.icmp6.ICMPv6EchoRequest;
-import org.opennms.protocols.icmp6.ICMPv6Packet;
-import org.opennms.protocols.icmp6.ICMPv6Packet.Type;
-import org.opennms.protocols.icmp6.ICMPv6Socket;
-
+import org.opennms.protocols.icmp.*;
 
 
 public class TestInitialization {
 
-	public static void main(final String[] args) {
-		try {
-                    int id = (int)(Math.random()*Short.MAX_VALUE);
-                    long threadId = (long)(Math.random()*Integer.MAX_VALUE);
-                    
-			final ICMPv6Socket socket = new ICMPv6Socket((short)id);
-			
-			Runnable r = new Runnable() {
-			    public void run() {
-			        System.err.println("Starting receiver");
-			        while(true) {
-			            try {
+    public static void main(final String[] args) {
+        try {
+            int id = (int)(Math.random()*Short.MAX_VALUE);
+            long threadId = (long)(Math.random()*Integer.MAX_VALUE);
+
+            final IcmpSocket socket = new IcmpSocket((short)id);
+
+            Runnable r = new Runnable() {
+                public void run() {
+                    System.err.println("Starting receiver");
+                    while(true) {
+                        try {
                             processReply(socket);
                         } catch (IOException e) {
                             e.printStackTrace();
                             System.exit(1);
                         }
-			        }
-			    }
+                    }
+                }
 
-                private void processReply(final ICMPv6Socket socket)
-                        throws IOException {
-                    
+                private void processReply(final IcmpSocket socket) throws IOException {
                     System.err.println("Waiting for packet");
                     DatagramPacket responsePacket = socket.receive();
 
-			        ICMPv6Packet icmpPacket = new ICMPv6Packet(responsePacket.getData(), responsePacket.getOffset(), responsePacket.getLength());
-			        System.err.printf("Recieved packet of type %s\n", icmpPacket.getType());
-			        if (icmpPacket.getType() == Type.EchoReply) {
-			            ICMPv6EchoReply reply = new ICMPv6EchoReply(icmpPacket);
-			            double rtt = reply.getRoundTripTime()/1000.0;
-			            String host = responsePacket.getAddress().getHostAddress();
-			            System.err.printf("%d bytes from %s, icmp_seq=%d time=%f\n", responsePacket.getLength(), host, reply.getSequenceNumber(), rtt);
-			        }
+                    ICMPEchoPacket icmpPacket = new ICMPEchoPacket(responsePacket.getData());
+                    System.err.printf("Recieved packet of type %s\n", icmpPacket.getType());
+                    double rtt = icmpPacket.getPingRTT()/1000.0;
+                    String host = responsePacket.getAddress().getHostAddress();
+                    System.err.printf("%d bytes from %s, icmp_seq=%d time=%f\n", responsePacket.getLength(), host, icmpPacket.getSequenceId(), rtt);
                 }
-			};
-			
-                        System.err.println("Sending packet\n");
-                        try {
-                            socket.setTrafficClass(Integer.parseInt("011100", 2));
-                        } catch (final IOException e) {
-                            System.err.println("Failed to set traffic class.");
-                            e.printStackTrace();
-                        }
-                        try {
-                            socket.dontFragment();
-                        } catch (final IOException e) {
-                            System.err.println("Failed to set 'Don't Fragment' bit on socket.");
-                            e.printStackTrace();
-                        }
+            };
 
-                        Thread receiver = new Thread(r);
-			receiver.start();
+            Thread receiver = new Thread(r);
+            receiver.start();
 
-			for(int seqNum = 0; seqNum < 10; seqNum++) {
-			
-			    ICMPv6EchoRequest request = new ICMPv6EchoRequest(id, seqNum, threadId);
-			
-			    byte[] bytes = request.toBytes();
-			    DatagramPacket packet = new DatagramPacket(bytes, 0, bytes.length, InetAddress.getByName("::1"), 0);
-            
-			    socket.send(packet);
-			    
-			    Thread.sleep(1000);
-			}
-			
-			
-		} catch (final Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		System.exit(0);
-	}
+            System.err.println("id=" + id + ", threadId=" + threadId);
+            for(int seqNum = 0; seqNum < 10; seqNum++) {
+                ICMPEchoPacket request = new ICMPEchoPacket(threadId);
+
+                byte[] bytes = request.toBytes();
+                DatagramPacket packet = new DatagramPacket(bytes, 0, bytes.length, InetAddress.getByName("127.0.0.1"), id);
+
+                System.err.println("Sending packet\n");
+                socket.setTrafficClass(46); // expedited forwarding
+                socket.dontFragment();
+                socket.send(packet);
+
+                Thread.sleep(1000);
+            }
+
+
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.exit(0);
+    }
 
 }
